8758442be6745c33b3aa03226b027a6216ec0068
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-06-09 01:08:27 -0300

    se guarda el historial de jugadas

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,21 +1,94 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './TicTacToe.css';
-import Board from './Board'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./TicTacToe.css";
+import Board from "./Board";
 class TicTacToe extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+
+    //create a new copy of the squares array
+    const squares = current.squares.slice();
+    //ignores a click if someone has won the game or if there is already an X or O in the square
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]), //stores the state of the board, will allow to determine who is the winner
+      xIsNext: !this.state.xIsNext, //switch turn
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
-      );
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TicTacToe;
+
+
+//====================================
+//helper that declares a winner
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
-  
-export default TicTacToe;
+  return null;
+}
