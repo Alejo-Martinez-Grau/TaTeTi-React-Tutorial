048dafccb79b2aaa034ebeb96e27b0763e130222
Alejo Martinez Grau <alejomartinezgrau@gmail.com>
2021-06-09 00:27:49 -0300

    Ta te ti v1 andando

--- src/Board.js
+++ src/Board.js
@@ -12,10 +12,15 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        //ignores a click if someone has won the game or if there is already an X or O in the square
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
+          squares: squares, //stores the state of the board, will allow to determine who is the winner
+          xIsNext: !this.state.xIsNext, //switch turn
         });
     }
 
@@ -24,8 +29,16 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-  
+      const winner = calculateWinner(this.state.squares);
+
+      let status;
+      if (winner){
+        status = `Winner: ${winner}`;
+      } else {
+        status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      }
+      
+      
       return (
         <div>
           <div className="status">{status}</div>
@@ -50,3 +63,26 @@ class Board extends React.Component {
   }
 
 export default Board;
+
+//====================================
+//helper that declares a winner
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
